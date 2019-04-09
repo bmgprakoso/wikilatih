1dd94d06390ca711f882890a93ec808678e02af6
Bambang Adi Prakoso <bambang.adi@ruma.co.id>
2019-04-09 20:33:36 +0700

    initiate new pages

--- application/routes.py
+++ application/routes.py
@@ -4,6 +4,21 @@ from flask import render_template
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html',)
+
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+
+@app.route('/trainings')
+def trainings():
+    return render_template('trainings.html')
+
+
+@app.route('/profile')
+def profile():
+    return render_template('profile.html')
 
 
