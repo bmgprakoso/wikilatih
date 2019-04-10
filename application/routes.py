4a74e3ae463c4e244a5ea8609086f32fd38d99fd
Bambang Adi Prakoso <bambang.adi@ruma.co.id>
2019-04-10 19:22:09 +0700

    refine navbar

--- application/routes.py
+++ application/routes.py
@@ -7,17 +7,22 @@ def index():
     return render_template('index.html')
 
 
-@app.route('/login')
+@app.route('/login/')
 def login():
     return render_template('login.html')
 
 
-@app.route('/trainings')
+@app.route('/register/')
+def register():
+    return render_template('register.html')
+
+
+@app.route('/trainings/')
 def trainings():
     return render_template('trainings.html')
 
 
-@app.route('/profile')
+@app.route('/profile/')
 def profile():
     return render_template('profile.html')
 
