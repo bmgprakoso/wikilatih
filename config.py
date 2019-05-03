b4d96c93a679639bc5c0be772aeff991d6a4ac2a
Bambang Adi Prakoso <bambang.adi@ruma.co.id>
2019-05-03 18:05:34 +0700

    Connect to db later, also change db title

--- config.py
+++ config.py
@@ -5,5 +5,6 @@ class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'cb6f2902-fce9-41c5-bb03-29f4b0f2b9c6'
     MONGODB_SETTINGS = {
         'db': 'wikilatih',
-        'host': 'mongodb+srv://wikilatih:wikilatih1234@cluster0-hotgr.mongodb.net/test?retryWrites=true'
+        'host': 'mongodb+srv://wikilatih:wikilatih1234@cluster0-hotgr.mongodb.net/wikilatih?retryWrites=true',
+        'connect': False
     }
