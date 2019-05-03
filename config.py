37c26b366db23ad539b5558ad694abe7ea3032d7
Bambang Adi Prakoso <bambang.adi@ruma.co.id>
2019-05-03 16:40:45 +0700

    Connect to cloud mongodb

--- config.py
+++ config.py
@@ -3,4 +3,7 @@ import os
 
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'cb6f2902-fce9-41c5-bb03-29f4b0f2b9c6'
-    MONGODB_SETTINGS = {'db': 'wikilatih'}
+    MONGODB_SETTINGS = {
+        'db': 'wikilatih',
+        'host': 'mongodb+srv://wikilatih:wikilatih1234@cluster0-hotgr.mongodb.net/test?retryWrites=true'
+    }
