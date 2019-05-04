36665ba3c6bd97799230a5661c4fd169c1bb1bd0
Bambang Adi Prakoso <bambang.adi@ruma.co.id>
2019-05-04 10:58:02 +0700

    remove bower

--- application/__init__.py
+++ application/__init__.py
@@ -1,13 +1,11 @@
 from flask import Flask
 from config import Config
 from flask_mongoengine import MongoEngine
-from flask_bower import Bower
 
 app = Flask(__name__)
 app.config.from_object(Config)
 
 db = MongoEngine()
 db.init_app(app)
-Bower(app)
 
 from application import routes
