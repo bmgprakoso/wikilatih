f8c2e56a60299b3804888acf127060b5e144b3de
Bambang Adi Prakoso <bambang.adi@ruma.co.id>
2019-05-05 16:16:00 +0700

    Implement dummy chart for training detail

---
+++ application/static/js/randomizeChart.js
@@ -0,0 +1,67 @@
+var randomScalingFactor = function () {
+    return Math.round(Math.random() * 100);
+};
+
+const getConfig = (type, dataArray) => {
+    const backgroundColor = dataArray.map(_ => '#' + (Math.random() * 0xFFFFFF << 0).toString(16));
+    const labels = dataArray.map((_, i) => `Label ${i}`);
+    return {
+        type: type,
+        data: {
+            datasets: [{
+                data: dataArray,
+                backgroundColor: backgroundColor,
+                label: 'Dataset'
+            }],
+            labels: labels
+        },
+        options: {
+            responsive: true
+        },
+    };
+};
+
+window.onload = function () {
+    var chartEvaluation1 = document.getElementById('chart-evaluation-1').getContext('2d');
+    var chartEvaluation2 = document.getElementById('chart-evaluation-2').getContext('2d');
+    var chartEvaluation3 = document.getElementById('chart-evaluation-3').getContext('2d');
+    var chartEvaluation4 = document.getElementById('chart-evaluation-4').getContext('2d');
+
+    window.myPie = new Chart(chartEvaluation1, getConfig('pie', [
+        randomScalingFactor(),
+        randomScalingFactor(),
+    ]));
+    window.myPie2 = new Chart(chartEvaluation2, getConfig('pie', [
+        randomScalingFactor(),
+        randomScalingFactor(),
+    ]));
+    window.myPie3 = new Chart(chartEvaluation3, getConfig('bar', [
+        randomScalingFactor(),
+        randomScalingFactor(),
+        randomScalingFactor(),
+        randomScalingFactor(),
+        randomScalingFactor(),
+        randomScalingFactor(),
+        randomScalingFactor(),
+        randomScalingFactor(),
+        randomScalingFactor(),
+        randomScalingFactor(),
+        randomScalingFactor(),
+    ]));
+    window.myPie4 = new Chart(chartEvaluation4, getConfig('line', [
+        randomScalingFactor(),
+        randomScalingFactor(),
+        randomScalingFactor(),
+        randomScalingFactor(),
+        randomScalingFactor(),
+        randomScalingFactor(),
+        randomScalingFactor(),
+        randomScalingFactor(),
+        randomScalingFactor(),
+        randomScalingFactor(),
+        randomScalingFactor(),
+        randomScalingFactor(),
+        randomScalingFactor(),
+        randomScalingFactor(),
+    ]));
+};
