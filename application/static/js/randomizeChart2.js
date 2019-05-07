fc773acdc030f56c6b8292ebf415ecffc667844d
Bambang Adi Prakoso <bambang.adi@ruma.co.id>
2019-05-07 20:47:29 +0700

    Refine dashboard page for admin

--- application/static/js/randomizeChart2.js
+++ application/static/js/randomizeChart2.js
@@ -22,80 +22,9 @@ const getConfig = (type, dataArray) => {
 };
 
 window.onload = function () {
-    var chartEvaluation1 = document.getElementById('chart-evaluation-1').getContext('2d');
-    var chartEvaluation2 = document.getElementById('chart-evaluation-2').getContext('2d');
-    var chartEvaluation3 = document.getElementById('chart-evaluation-3').getContext('2d');
-    var chartEvaluation4 = document.getElementById('chart-evaluation-4').getContext('2d');
-    var chartEvaluation5 = document.getElementById('chart-evaluation-5').getContext('2d');
-    var chartEvaluation6 = document.getElementById('chart-evaluation-6').getContext('2d');
-
-    window.myPie = new Chart(chartEvaluation1, getConfig('pie', [
-        randomScalingFactor(),
-        randomScalingFactor(),
-    ]));
-    window.myPie2 = new Chart(chartEvaluation2, getConfig('pie', [
-        randomScalingFactor(),
-        randomScalingFactor(),
-    ]));
-    window.myPie3 = new Chart(chartEvaluation3, getConfig('bar', [
-        randomScalingFactor(),
-        randomScalingFactor(),
-        randomScalingFactor(),
-        randomScalingFactor(),
-        randomScalingFactor(),
-        randomScalingFactor(),
-        randomScalingFactor(),
-        randomScalingFactor(),
-        randomScalingFactor(),
-        randomScalingFactor(),
-        randomScalingFactor(),
-    ]));
-    window.myPie4 = new Chart(chartEvaluation4, getConfig('line', [
-        randomScalingFactor(),
-        randomScalingFactor(),
-        randomScalingFactor(),
-        randomScalingFactor(),
-        randomScalingFactor(),
-        randomScalingFactor(),
-        randomScalingFactor(),
-        randomScalingFactor(),
-        randomScalingFactor(),
-        randomScalingFactor(),
-        randomScalingFactor(),
-        randomScalingFactor(),
-        randomScalingFactor(),
-        randomScalingFactor(),
-    ]));
-    window.myPie5 = new Chart(chartEvaluation5, getConfig('line', [
-        randomScalingFactor(),
-        randomScalingFactor(),
-        randomScalingFactor(),
-        randomScalingFactor(),
-        randomScalingFactor(),
-        randomScalingFactor(),
-        randomScalingFactor(),
-        randomScalingFactor(),
-        randomScalingFactor(),
-        randomScalingFactor(),
-        randomScalingFactor(),
-        randomScalingFactor(),
-        randomScalingFactor(),
-        randomScalingFactor(),
-    ]));
-    window.myPie6 = new Chart(chartEvaluation6, getConfig('line', [
-        randomScalingFactor(),
-        randomScalingFactor(),
-        randomScalingFactor(),
-        randomScalingFactor(),
-        randomScalingFactor(),
-        randomScalingFactor(),
-        randomScalingFactor(),
-        randomScalingFactor(),
-        randomScalingFactor(),
-        randomScalingFactor(),
-        randomScalingFactor(),
-        randomScalingFactor(),
-        randomScalingFactor(),
-        randomScalingFactor(),
-    ]));
+    window.myPie = new Chart(document.getElementById('chart-clarity').getContext('2d'), getConfig('pie', Array(2).fill(randomScalingFactor())));
+    window.myPie2 = new Chart(document.getElementById('chart-retention').getContext('2d'), getConfig('pie', Array(2).fill(randomScalingFactor())));
+    window.myPie3 = new Chart(document.getElementById('chart-dau').getContext('2d'), getConfig('bar', Array(10).fill(randomScalingFactor())));
+    window.myPie4 = new Chart(document.getElementById('chart-test-score').getContext('2d'), getConfig('line', Array(10).fill(randomScalingFactor())));
+    window.myPie5 = new Chart(document.getElementById('chart-wikimedia-project').getContext('2d'), getConfig('bar', Array(10).fill(randomScalingFactor())));
 };
